47443355225b5dddf40ba51bafecee7f3a2eec1e
parameswari.bala <parameswaribala@gmail.com>
2023-10-16 17:27:22 +0530

    updated

---
+++ loanapp/src/main/java/com/boa/loanapp/controllers/LoanProcessController.java
@@ -0,0 +1,35 @@
+package com.boa.loanapp.controllers;
+
+import io.camunda.zeebe.client.ZeebeClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/loans")
+@Slf4j
+public class LoanProcessController {
+
+    private ZeebeClient zeebeClient;
+
+   @PostMapping("/v1.0/")
+    public void startBPMNProcess(@RequestBody Map<String, Object> variables){
+
+       log.info(
+               "Starting process `" + ProcessConstants.BPMN_PROCESS_ID + "` with variables: " + variables);
+
+       zeebeClient
+               .newCreateInstanceCommand()
+               .bpmnProcessId(ProcessConstants.BPMN_PROCESS_ID)
+               .latestVersion()
+               .variables(variables)
+               .send();
+
+    }
+
+
+}
---
+++ loanapp/src/main/java/com/boa/loanapp/controllers/ProcessConstants.java
@@ -0,0 +1,6 @@
+package com.boa.loanapp.controllers;
+
+public class ProcessConstants {
+
+    public static  final String BPMN_PROCESS_ID="Process_Loan";
+}
