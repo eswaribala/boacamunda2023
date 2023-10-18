d891c180af4ca412002a42a798d371adc126e3e1
parameswari.bala <parameswaribala@gmail.com>
2023-10-18 14:58:36 +0530

    updated

--- camundaconsoleproject/camundaconsoleproject/src/main/java/com/boa/camundaconsoleproject/configurations/JobConfiguration.java
+++ camundaconsoleproject/camundaconsoleproject/src/main/java/com/boa/camundaconsoleproject/configurations/JobConfiguration.java
@@ -5,10 +5,14 @@ import java.util.Map;
 
 import org.springframework.context.annotation.Configuration;
 
+import com.github.wnameless.json.flattener.JsonFlattener;
+
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
 import lombok.extern.slf4j.Slf4j;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
 
 @Configuration
 @Slf4j
@@ -22,11 +26,22 @@ public class JobConfiguration {
 		Iterator<Map.Entry<String,Object>> itr=map.entrySet().iterator();
 		
 		Map.Entry<String,Object> entry=null;
+		Map<String,Object> result=null;
 		while(itr.hasNext()) {
 			entry=itr.next();
-			log.info(entry.getKey(),entry.getValue());			
+			System.out.println(entry.getKey()+","+entry.getValue());
+			result=parseString(entry.getValue().toString());
 		}		
 
+		itr=result.entrySet().iterator();
+		entry=null;	
+		while(itr.hasNext()) {
+			entry=itr.next();
+			System.out.println(entry.getKey()+","+entry.getValue());
+			
+		}		
+		
+		
         jobClient.newCompleteCommand(activatedJob.getKey())
                // .variables(map)
                 .send()
@@ -40,5 +55,39 @@ public class JobConfiguration {
 		
 	}
 	
+	
+	//response string to object and separates the token
+
+    private static Map<String, Object> parseString(String response)
+    {
+        JSONParser parser = new JSONParser();
+        Map<String, Object> flattenedJsonMap=null;
+        String token="";
+        try {
+
+            // Put above JSON content to crunchify.txt file and change path location
+            Object obj = parser.parse(response);
+            JSONObject jsonObject = (JSONObject) obj;
+
+            // JsonFlattener: A Java utility used to FLATTEN nested JSON objects
+            String flattenedJson = JsonFlattener.flatten(jsonObject.toString());
+            //log("\n=====Simple Flatten===== \n" + flattenedJson);
+
+            flattenedJsonMap= JsonFlattener.flattenAsMap(jsonObject.toString());
+              token=(String) flattenedJsonMap.get("body");
+            //log("\n=====Flatten As Map=====\n" + flattenedJson);
+            // We are using Java8 forEach loop. More info: https://crunchify.com/?p=8047
+            //flattenedJsonMap.forEach((k, v) -> log(k + " : " + v));
+
+            // Unflatten it back to original JSON
+           // String nestedJson = JsonUnflattener.unflatten(flattenedJson);
+           // System.out.println("\n=====Unflatten it back to original JSON===== \n" + nestedJson);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return flattenedJsonMap;
+
+    }
+
 	
 }
